c15fd265509935ad79a26354f650a861e408136f
Erik Jansen <erik.gunnar.jansen@nav.no>
2022-01-10 10:53:21 +0100

    Bump dependency and plugin versions to latest

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,22 +5,22 @@ group = "no.nav.syfo"
 version = "0.0.1"
 
 object Versions {
-    const val ktor = "1.6.5"
-    const val kotlin = "1.5.31"
-    const val logback = "1.2.6"
-    const val logstashEncoder = "6.6"
-    const val hikari = "5.0.0"
+    const val ktor = "1.6.7"
+    const val kotlin = "1.6.10"
+    const val logback = "1.2.10"
+    const val logstashEncoder = "7.0.1"
+    const val hikari = "5.0.1"
     const val postgres = "42.3.1"
-    const val flyway = "8.0.2"
+    const val flyway = "8.4.0"
     const val kluent = "1.68"
     const val spek = "2.0.17"
     const val pgEmbedded = "0.13.4"
-    const val micrometerRegistry = "1.7.5"
+    const val micrometerRegistry = "1.8.1"
 }
 plugins {
-    kotlin("jvm") version "1.5.31"
-    id("com.github.johnrengelman.shadow") version "7.1.0"
-    id("org.jlleitschuh.gradle.ktlint") version "10.2.0"
+    kotlin("jvm") version "1.6.10"
+    id("com.github.johnrengelman.shadow") version "7.1.2"
+    id("org.jlleitschuh.gradle.ktlint") version "10.2.1"
 }
 
 repositories {
