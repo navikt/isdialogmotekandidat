ed7473aa1165d96d61cd203132052f385a025c15
Erik Jansen <erik.gunnar.jansen@nav.no>
2022-04-12 10:08:11 +0200

    Bump Kotlin version to latest

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,7 +23,7 @@ object Versions {
     const val spek = "2.0.18"
 }
 plugins {
-    kotlin("jvm") version "1.6.10"
+    kotlin("jvm") version "1.6.20"
     id("com.github.johnrengelman.shadow") version "7.1.2"
     id("org.jlleitschuh.gradle.ktlint") version "10.2.1"
 }
