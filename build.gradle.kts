23bb55e34cd41c759d7baf58e411c2fd66807da9
Erik Jansen <erik.gunnar.jansen@nav.no>
2022-05-02 12:09:11 +0200

    Bump dependency versions to latest
    Bump Gradle, Flyway, Jackson, Kotlin and Ktor version to latest

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,14 +6,14 @@ version = "0.0.1"
 
 object Versions {
     const val confluent = "7.1.0"
-    const val flyway = "8.5.9"
+    const val flyway = "8.5.10"
     const val hikari = "5.0.1"
     const val isdialogmoteSchema = "1.0.5"
-    const val jackson = "2.13.1"
+    const val jackson = "2.13.2"
     const val kafka = "3.1.0"
     const val kafkaEmbedded = "3.1.0"
     const val kluent = "1.68"
-    const val ktor = "2.0.0"
+    const val ktor = "2.0.1"
     const val logback = "1.2.11"
     const val logstashEncoder = "7.1.1"
     const val micrometerRegistry = "1.8.5"
@@ -24,7 +24,7 @@ object Versions {
     const val spek = "2.0.18"
 }
 plugins {
-    kotlin("jvm") version "1.6.20"
+    kotlin("jvm") version "1.6.21"
     id("com.github.johnrengelman.shadow") version "7.1.2"
     id("org.jlleitschuh.gradle.ktlint") version "10.2.1"
 }
