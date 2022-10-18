7f0eee80b3494415ca497a0e7fd5fc4b0241fac4
Geir Arne Waagbø <79845894+geir-waagboe@users.noreply.github.com>
2022-10-18 10:38:23 +0200

    Use ktor retry to avoid ClosedReceiveChannelException (#83)

--- src/main/kotlin/no/nav/syfo/client/HttpClientCommon.kt
+++ src/main/kotlin/no/nav/syfo/client/HttpClientCommon.kt
@@ -1,26 +1,31 @@
 package no.nav.syfo.client
 
 import io.ktor.client.*
+import io.ktor.client.engine.*
 import io.ktor.client.engine.apache.*
 import io.ktor.client.engine.cio.*
+import io.ktor.client.plugins.*
 import io.ktor.client.plugins.contentnegotiation.*
 import io.ktor.serialization.jackson.*
 import no.nav.syfo.util.*
 import org.apache.http.impl.conn.SystemDefaultRoutePlanner
 import java.net.ProxySelector
 
-fun httpClientDefault() = HttpClient(CIO) {
+val commonConfig: HttpClientConfig<out HttpClientEngineConfig>.() -> Unit = {
     install(ContentNegotiation) {
         jackson { configure() }
+    }
+    install(HttpRequestRetry) {
+        retryOnExceptionIf(2) { _, cause ->
+            cause !is ClientRequestException
+        }
+        constantDelay(500L)
     }
     expectSuccess = true
 }
 
 val proxyConfig: HttpClientConfig<ApacheEngineConfig>.() -> Unit = {
-    install(ContentNegotiation) {
-        jackson { configure() }
-    }
-    expectSuccess = true
+    this.commonConfig()
     engine {
         customizeClient {
             setRoutePlanner(SystemDefaultRoutePlanner(ProxySelector.getDefault()))
@@ -28,7 +33,5 @@ val proxyConfig: HttpClientConfig<ApacheEngineConfig>.() -> Unit = {
     }
 }
 
-fun httpClientProxy() = HttpClient(
-    engineFactory = Apache,
-    block = proxyConfig,
-)
+fun httpClientDefault() = HttpClient(CIO, commonConfig)
+fun httpClientProxy() = HttpClient(Apache, proxyConfig)
