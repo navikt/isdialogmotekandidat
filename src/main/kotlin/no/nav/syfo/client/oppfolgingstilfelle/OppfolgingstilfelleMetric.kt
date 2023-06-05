4dda8df14c291b26715e812481ba3dea3326b628
Anders Rognstad <79838644+andersrognstad@users.noreply.github.com>
2023-06-05 14:11:06 +0200

    Add timers for calls to veiledertilgangskontroll persons and oppfolgingstilfeller for unntak (#125)

--- src/main/kotlin/no/nav/syfo/client/oppfolgingstilfelle/OppfolgingstilfelleMetric.kt
+++ src/main/kotlin/no/nav/syfo/client/oppfolgingstilfelle/OppfolgingstilfelleMetric.kt
@@ -1,6 +1,7 @@
 package no.nav.syfo.client.oppfolgingstilfelle
 
 import io.micrometer.core.instrument.Counter
+import io.micrometer.core.instrument.Timer
 import no.nav.syfo.application.metric.METRICS_NS
 import no.nav.syfo.application.metric.METRICS_REGISTRY
 
@@ -12,9 +13,15 @@ const val CALL_OPPFOLGINGSTILFELLE_PERSON_FAIL = "${CALL_OPPFOLGINGSTILFELLE_PER
 
 val COUNT_CALL_OPPFOLGINGSTILFELLE_PERSON_SUCCESS: Counter = Counter
     .builder(CALL_OPPFOLGINGSTILFELLE_PERSON_SUCCESS)
-    .description("Counts the number of successful calls to Isoppfolgingstilfelle - OppfolgingstifellePerson")
+    .description("Counts the number of successful calls to Isoppfolgingstilfelle - OppfolgingstilfellePerson")
     .register(METRICS_REGISTRY)
 val COUNT_CALL_OPPFOLGINGSTILFELLE_PERSON_FAIL: Counter = Counter
     .builder(CALL_OPPFOLGINGSTILFELLE_PERSON_FAIL)
-    .description("Counts the number of failed calls to Isoppfolgingstilfelle - OppfolgingstifellePerson")
+    .description("Counts the number of failed calls to Isoppfolgingstilfelle - OppfolgingstilfellePerson")
+    .register(METRICS_REGISTRY)
+
+const val CALL_OPPFOLGINGSTILFELLER_UNNTAK_TIMER = "${CALL_OPPFOLGINGSTILFELLE_BASE}_timer"
+val HISTOGRAM_CALL_OPPFOLGINGSTILFELLER_UNNTAK_TIMER: Timer = Timer
+    .builder(CALL_OPPFOLGINGSTILFELLER_UNNTAK_TIMER)
+    .description("Timer for calls to get oppfolgingstilfeller for unntak")
     .register(METRICS_REGISTRY)
--- src/main/kotlin/no/nav/syfo/unntak/UnntakService.kt
+++ src/main/kotlin/no/nav/syfo/unntak/UnntakService.kt
@@ -2,6 +2,7 @@ package no.nav.syfo.unntak
 
 import no.nav.syfo.application.database.DatabaseInterface
 import no.nav.syfo.application.exception.ConflictException
+import no.nav.syfo.client.oppfolgingstilfelle.HISTOGRAM_CALL_OPPFOLGINGSTILFELLER_UNNTAK_TIMER
 import no.nav.syfo.dialogmotekandidat.DialogmotekandidatService
 import no.nav.syfo.dialogmotekandidat.database.getDialogmotekandidatEndringListForPerson
 import no.nav.syfo.dialogmotekandidat.database.toDialogmotekandidatEndringList
@@ -14,6 +15,7 @@ import no.nav.syfo.unntak.database.*
 import no.nav.syfo.unntak.database.domain.toUnntakList
 import no.nav.syfo.unntak.domain.*
 import no.nav.syfo.util.toLocalDateOslo
+import java.time.Duration
 import java.util.concurrent.ConcurrentHashMap
 
 class UnntakService(
@@ -63,9 +65,34 @@ class UnntakService(
         token: String,
         callId: String,
     ): List<UnntakStatistikk> {
+        val starttime = System.currentTimeMillis()
+        val oppfolgingstilfelleForUnntak = getOppfolgingstilfelleForUnntak(
+            unntakList = unntakList,
+            token = token,
+            callId = callId
+        )
+        val duration = Duration.ofMillis(System.currentTimeMillis() - starttime)
+        HISTOGRAM_CALL_OPPFOLGINGSTILFELLER_UNNTAK_TIMER.record(duration)
+
+        return oppfolgingstilfelleForUnntak.mapNotNull { (unntak, oppfolgingstilfelle) ->
+            oppfolgingstilfelle?.let { tilfelle ->
+                UnntakStatistikk(
+                    unntakDato = unntak.createdAt.toLocalDateOslo(),
+                    tilfelleStart = tilfelle.tilfelleStart,
+                    tilfelleEnd = tilfelle.tilfelleEnd,
+                )
+            }
+        }
+    }
+
+    private suspend fun getOppfolgingstilfelleForUnntak(
+        unntakList: List<Unntak>,
+        token: String,
+        callId: String,
+    ): Map<Unntak, Oppfolgingstilfelle?> {
         val personIdentOppfolgingstilfellerMap = ConcurrentHashMap<PersonIdentNumber, List<Oppfolgingstilfelle>>()
 
-        return unntakList.mapNotNull { unntak ->
+        return unntakList.associateWith { unntak ->
             val tilfellerForUnntakPerson = personIdentOppfolgingstilfellerMap.getOrPut(unntak.personIdent) {
                 oppfolgingstilfelleService.getAllOppfolgingstilfeller(
                     arbeidstakerPersonIdent = unntak.personIdent,
@@ -76,13 +103,7 @@ class UnntakService(
 
             val unntakDato = unntak.createdAt.toLocalDateOslo()
             val tilfelleForUnntak = tilfellerForUnntakPerson.tilfelleForDate(unntakDato)
-            tilfelleForUnntak?.let { tilfelle ->
-                UnntakStatistikk(
-                    unntakDato = unntakDato,
-                    tilfelleStart = tilfelle.tilfelleStart,
-                    tilfelleEnd = tilfelle.tilfelleEnd,
-                )
-            }
+            tilfelleForUnntak
         }
     }
 
