a8ef807ffe56f15860c9a632fea59c5e9f61bfa3
Anders Rognstad <79838644+andersrognstad@users.noreply.github.com>
2024-10-28 14:41:36 +0100

    IS-2752: Add endpoint for historikk (#182)
    * IS-2752: Add endpoint for historikk
    
    * Rename fields

--- src/main/kotlin/no/nav/syfo/ikkeaktuell/IkkeAktuellService.kt
+++ src/main/kotlin/no/nav/syfo/ikkeaktuell/IkkeAktuellService.kt
@@ -6,13 +6,17 @@ import no.nav.syfo.dialogmotekandidat.DialogmotekandidatService
 import no.nav.syfo.dialogmotekandidat.database.getDialogmotekandidatEndringListForPerson
 import no.nav.syfo.dialogmotekandidat.database.toDialogmotekandidatEndringList
 import no.nav.syfo.dialogmotekandidat.domain.*
+import no.nav.syfo.domain.PersonIdentNumber
+import no.nav.syfo.ikkeaktuell.database.IkkeAktuellRepository
 import no.nav.syfo.ikkeaktuell.database.createIkkeAktuell
+import no.nav.syfo.ikkeaktuell.database.toIkkeAktuellList
 import no.nav.syfo.ikkeaktuell.domain.IkkeAktuell
 import no.nav.syfo.oppfolgingstilfelle.OppfolgingstilfelleService
 
 class IkkeAktuellService(
     private val database: DatabaseInterface,
     private val dialogmotekandidatService: DialogmotekandidatService,
+    private val ikkeAktuellRepository: IkkeAktuellRepository,
     private val oppfolgingstilfelleService: OppfolgingstilfelleService,
 ) {
     suspend fun createIkkeAktuell(
@@ -47,4 +51,7 @@ class IkkeAktuellService(
             connection.commit()
         }
     }
+
+    fun getIkkeAktuellList(personIdent: PersonIdentNumber): List<IkkeAktuell> =
+        ikkeAktuellRepository.getIkkeAktuellListForPerson(personIdent = personIdent).toIkkeAktuellList()
 }
