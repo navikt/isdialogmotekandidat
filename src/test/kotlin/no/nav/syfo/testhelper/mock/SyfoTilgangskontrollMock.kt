be29701fb59fa883630c385a01fd422d500b3cb6
eirikdahlen <dahleneirik@gmail.com>
2025-12-18 22:24:38 +0100

    IS-3362: Add bulk tilgangskontroll

--- src/test/kotlin/no/nav/syfo/testhelper/mock/SyfoTilgangskontrollMock.kt
+++ src/test/kotlin/no/nav/syfo/testhelper/mock/SyfoTilgangskontrollMock.kt
@@ -2,13 +2,27 @@ package no.nav.syfo.testhelper.mock
 
 import io.ktor.client.engine.mock.*
 import io.ktor.client.request.*
+import kotlinx.coroutines.runBlocking
 import no.nav.syfo.infrastructure.clients.veiledertilgang.Tilgang
+import no.nav.syfo.infrastructure.clients.veiledertilgang.VeilederTilgangskontrollClient
 import no.nav.syfo.testhelper.UserConstants.PERSONIDENTNUMBER_VEILEDER_NO_ACCESS
 import no.nav.syfo.util.NAV_PERSONIDENT_HEADER
 
 fun MockRequestHandleScope.isTilgangskontrollResponse(request: HttpRequestData): HttpResponseData {
-    return when (request.headers[NAV_PERSONIDENT_HEADER]) {
-        PERSONIDENTNUMBER_VEILEDER_NO_ACCESS.value -> respond(Tilgang(erGodkjent = false))
-        else -> respond(Tilgang(erGodkjent = true))
+    val requestUrl = request.url.encodedPath
+
+    return when {
+        requestUrl.endsWith(VeilederTilgangskontrollClient.TILGANGSKONTROLL_PERSON_PATH) -> {
+            when (request.headers[NAV_PERSONIDENT_HEADER]) {
+                PERSONIDENTNUMBER_VEILEDER_NO_ACCESS.value -> respond(Tilgang(erGodkjent = false))
+                else -> respond(Tilgang(erGodkjent = true))
+            }
+        }
+        requestUrl.endsWith(VeilederTilgangskontrollClient.TILGANGSKONTROLL_BRUKERE_PATH) -> {
+            val body = runBlocking<List<String>> { request.receiveBody() }.toMutableList()
+            body.removeAll { it == PERSONIDENTNUMBER_VEILEDER_NO_ACCESS.value }
+            respond(body)
+        }
+        else -> error("Unhandled path $requestUrl")
     }
 }
